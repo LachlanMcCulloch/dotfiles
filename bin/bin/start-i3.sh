a8fc42295912136424ac03f2698434d82f8edad1
Lachlan McCulloch <ltjmcculloch@gmail.com>
2018-07-11 18:05:48 +1000

    support for bad setup to fail on start-up

--- bin/bin/start-i3.sh
+++ bin/bin/start-i3.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
-~/.i3/i3-create-config/create_config
-# Start i3
-exec i3
+DROPBOX_DAEMON=~/.dropbox-dist/dropboxd
+
+if [ -f $DROPBOX_DAEMON ]; then
+    $DROPBOX_DAEMON &
+fi
+
+I3_CONFIG_CREATE=~/.i3/i3-create-config/create_config
+
+if [ -f $I3_CONFIG_CREATE ]; then
+    $I3_CONFIG_CREATE
+    # Start i3
+    exec i3
+else
+    exit 1
+fi
+
+
